6796a2b76b5904510d0195c6ea944ea528bd7277
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-15 01:53:00 +0300

    more sucess

--- main.js
+++ main.js
@@ -1,67 +1,62 @@
-'use strict';
+"use strict";
 
-// us calcualtion 
+// us calcualtion
 var totalUs = document.getElementById("us-total");
 var inputUs = document.getElementById("us");
 
 // Them calculation
 var totalThem = document.getElementById("them-total");
 var inputThem = document.getElementById("them");
-// record btn 
+// record btn
 var calcBtn = document.getElementById("calc");
-// this function will get the data from us input filed and save it to currentForUs var // 
+// this function will get the data from us input filed and save it to currentForUs var //
 var usHistory = document.getElementById("history-them");
 var themHistory = document.getElementById("history-us");
-//  take the input value after user change the save to var 
+//  take the input value after user change the save to var
+
+inputThem.addEventListener("input", function () {
+  if (inputThem.value > 9) {
+    if (inputUs.value == "") inputUs.focus();
+  }
+});
+inputUs.addEventListener("input", function () {
+  if (inputUs.value > 9) {
+    if (inputThem.value == "") inputThem.focus();
+  }
+});
+
+calcBtn.addEventListener("click", function () {
 
 
-inputThem.addEventListener("input", function(){
+  if (inputThem.value == "") {
+    inputThem.value = 0;
+  }
+  if (inputUs.value == "") {
+    inputUs.value = 0;
+  }
+  totalUs.innerText = parseInt(inputUs.value) + parseInt(totalUs.innerText);
+  totalThem.innerText =
+    parseInt(inputThem.value) + parseInt(totalThem.innerText);
 
-    if(inputThem.value >9){
-        inputUs.focus();
-    }
+if(inputUs.value > 0){
+  var last = document.createElement("h1");
+  var textLast = document.createTextNode(parseInt(inputUs.value));
+  last.appendChild(textLast);
+  usHistory.appendChild(last);
+}
+if(inputThem.value > 0){
+  var last2 = document.createElement("h1");
+  var textLast2 = document.createTextNode(parseInt(inputThem.value));
+  last2.appendChild(textLast2);
+  themHistory.appendChild(last2);
+}
+  inputUs.value = "";
+  inputThem.value = "";
+});
 
-})
-inputUs.addEventListener("input", function(){
+// to creat h1 text with the value input from user inside the history
 
-    if(inputUs.value >9){
-        inputThem.focus();
-    }
-})
-
-
-calcBtn.addEventListener("click", function(){
-    if (inputThem.value == ""){
-        inputThem.value = 0
-    }
-    if (inputUs.value == ""){
-        inputUs.value = 0
-    }
-    totalUs.innerText = parseInt(inputUs.value) + parseInt(totalUs.innerText)
-    totalThem.innerText = parseInt(inputThem.value) + parseInt(totalThem.innerText)
-
-   
-     var last  =  document.createElement("h1");
-     var textLast =  document.createTextNode(parseInt(inputUs.value))
-    last.appendChild(textLast);
-    usHistory.appendChild(last);
-
-     var last2  =  document.createElement("h1");
-     var textLast2 =  document.createTextNode(parseInt(inputThem.value))
-    last2.appendChild(textLast2);
-    themHistory.appendChild(last2);
-
-    inputUs.value = "";
-    inputThem.value = "";
-    
-})
-
-
-// to creat h1 text with the value input from user inside the history 
-
-
-
-   /** switch button  */
+/** switch button  */
 // var switchBtn = document.querySelector("button");
 // var bodyTag = document.querySelectorAll("body");
 // var boxes = document.querySelectorAll(".box");
@@ -79,7 +74,7 @@ calcBtn.addEventListener("click", function(){
 //         coder[i].classList.toggle("coder-name-blue")
 //         coder[i].classList.toggle("coder-name-white")
 //     }
-    
+
 //     bodyTag[0].classList.toggle("background-black");
 //     bodyTag[0].classList.toggle("white-text");
 //     switchBtn.classList.toggle("switch-btn");
@@ -92,7 +87,7 @@ calcBtn.addEventListener("click", function(){
 //     for(i = 0; i < todayBoxes.length; i = i +1 ) {
 //         todayBoxes[i].classList.toggle("dark-mode")
 //     }
- 
+
 //    for (i = 0; midH1.length; i = i + 1){
 //     midH1[i].classList.toggle("black-text");
 //     midH1[i].classList.toggle("white-text");
@@ -100,8 +95,5 @@ calcBtn.addEventListener("click", function(){
 //      }
 //      overView.classList.toggle("gray-color")
 //      overView.classList.toggle("white-text")
-     
-
 
 // })
-
