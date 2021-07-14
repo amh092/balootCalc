87947013997b42c626c4fa9cb5bf8addde35c24e
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-15 01:40:14 +0300

    yasser switch

--- main.js
+++ main.js
@@ -15,6 +15,19 @@ var themHistory = document.getElementById("history-us");
 //  take the input value after user change the save to var 
 
 
+inputThem.addEventListener("input", function(){
+
+    if(inputThem.value >9){
+        inputUs.focus();
+    }
+
+})
+inputUs.addEventListener("input", function(){
+
+    if(inputUs.value >9){
+        inputThem.focus();
+    }
+})
 
 
 calcBtn.addEventListener("click", function(){
@@ -43,8 +56,52 @@ calcBtn.addEventListener("click", function(){
     
 })
 
+
 // to creat h1 text with the value input from user inside the history 
 
 
 
-   
+   /** switch button  */
+// var switchBtn = document.querySelector("button");
+// var bodyTag = document.querySelectorAll("body");
+// var boxes = document.querySelectorAll(".box");
+// var todayBoxes = document.querySelectorAll(".today-box");
+// var midH1 = document.querySelectorAll(".black-text")
+// var firstContainer = document.querySelector(".first-container")
+// var buttonSpan = document.querySelector("button span")
+// var overView = document.querySelector(".overview")
+// var coder = document.querySelectorAll(".coder");
+
+// toggling dark mode on and off based on button clicking
+
+// switchBtn.addEventListener("click", function () {
+//     for(i =0; i < coder.length; i = i +1){
+//         coder[i].classList.toggle("coder-name-blue")
+//         coder[i].classList.toggle("coder-name-white")
+//     }
+    
+//     bodyTag[0].classList.toggle("background-black");
+//     bodyTag[0].classList.toggle("white-text");
+//     switchBtn.classList.toggle("switch-btn");
+//     firstContainer.classList.toggle("dark-mode");
+//     buttonSpan.classList.toggle("mode-switch-light")
+//     buttonSpan.classList.toggle("switch-btn-span-dark")
+//     for(i = 0; i < boxes.length; i = i +1 ) {
+//         boxes[i].classList.toggle("dark-mode")
+//     }
+//     for(i = 0; i < todayBoxes.length; i = i +1 ) {
+//         todayBoxes[i].classList.toggle("dark-mode")
+//     }
+ 
+//    for (i = 0; midH1.length; i = i + 1){
+//     midH1[i].classList.toggle("black-text");
+//     midH1[i].classList.toggle("white-text");
+
+//      }
+//      overView.classList.toggle("gray-color")
+//      overView.classList.toggle("white-text")
+     
+
+
+// })
+
